33ad5dd7fdc7ddc0734b52cb5a8d57ebbf784207
Bastien <bastien69580@gmail.com>
2021-02-12 20:58:26 +0100

    Play + TMP

---
+++ PimpMyFusee/Assets/Scripts/Module.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+// Main script for the modules
+public class Module : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rigidbody2d = null;
+    bool activated = false;
+
+
+
+
+
+
+
+
+    private void Awake()                                                            // AWAKE
+    {
+        GetComponentsIfNotReferenced();
+    }
+
+    private void Start()                                                             // START
+    {
+        // When the module appears on screen it is not activated yet
+        TriggerModule(false);
+    }
+
+
+
+
+
+
+
+
+    // MODULE
+    public void TriggerModule(bool state = true)
+    {
+        rigidbody2d.isKinematic = !state;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    private void GetComponentsIfNotReferenced()
+    {
+        // GET COMPONENTS IS NOT REFERENCED
+        if (rigidbody2d == null)
+            if (GetComponent<Rigidbody2D>())
+                rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        GetComponentsIfNotReferenced();
+    }
+}
