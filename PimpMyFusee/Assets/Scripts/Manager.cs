8f7a5ce10a8b9b951d48cc36a0f0cd221595c853
Bastien <bastien69580@gmail.com>
2021-02-12 21:43:23 +0100

    Modules

--- PimpMyFusee/Assets/Scripts/Manager.cs
+++ PimpMyFusee/Assets/Scripts/Manager.cs
@@ -10,7 +10,7 @@ public class Manager : MonoBehaviour
 {
     [Tooltip("List of all the instanciated modules")]
     [SerializeField] List<Module> modulesList = new List<Module>();
-
+    [SerializeField] bool getAllModulesOnStart = false;
 
 
 
@@ -19,6 +19,19 @@ public class Manager : MonoBehaviour
     // STARTS THE GAME
     public void StartSimulation()
     {
+        // Find modules in scene
+        if (getAllModulesOnStart)
+        {
+            modulesList.Clear();
+
+            Module[] modulesArray = GameObject.FindObjectsOfType<Module>();
+            for (int i = 0; i < modulesArray.Length; i++)
+                modulesList.Add(modulesArray[i]);
+        }
+            
+
+
+        // Start modules
         if (modulesList != null && modulesList.Count > 0)
             for (int i = 0; i < modulesList.Count; i++)
                 if (modulesList[i] != null)
