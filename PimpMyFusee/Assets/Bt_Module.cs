c3fdeb2a4c54d3f888e0f2c75dc71552f08fa2b5
CluClu <hugocluzel17@hotmail.fr>
2021-02-12 22:23:00 +0100

    Bt_prefab + rotate

---
+++ PimpMyFusee/Assets/Bt_Module.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bt_Module : MonoBehaviour
+{
+    private ManagerShop ms;
+
+    [SerializeField] private GameObject prefabModule;
+
+    private Image icon;
+    private Button bt;
+    private Text text;
+
+    private void Awake()
+    {
+        ms = ManagerShop.Instance;
+
+        icon = GetComponent<Image>();
+        bt = GetComponent<Button>();
+        text = GetComponentInChildren<Text>();
+
+        text.text = prefabModule.name;
+        //icon.sprite = Resources.Load<Sprite>("Sprite/"+prefabModule.name);
+
+        bt.onClick.AddListener( delegate { ms.GetModule(prefabModule); });
+    }
+}
