dac470650e921b57d8040d0c5eb4d684dab23afc
Jules Rouault <trustprinciples@gmail.com>
2021-02-13 18:34:56 +0100

    La fusée pète le feu

---
+++ PimpMyFusee/Assets/Scripts/FX/ParticleEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEvent : MonoBehaviour
+{
+    ParticleSystem myParticleSystem;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myParticleSystem = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    public void CheckPlay()
+    {
+        if (!myParticleSystem.isPlaying)
+            myParticleSystem.Play();
+    }
+}
